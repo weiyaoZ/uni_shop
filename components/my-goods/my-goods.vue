3281478487f733d5d1fb060a407e24991166c1df
liangchen <liangchen190511@163.com>
2022-08-11 23:44:18 +0800

    完成了商品列表的开发

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,64 @@
+<template>
+	<view class="goods-item">
+	    <!-- 左侧的盒子 -->
+	    <view class="goods-item-left">
+	        <image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
+	    </view>
+	    <!-- 右侧的盒子 -->
+	    <view class="goods-item-right">
+	        <view class="goods-name">{{goods.goods_name}}</view>
+	        <view class="goods-info-box">
+	            <view class="goods-price">￥{{goods.goods_price}}.00</view>
+	        </view>
+	    </view>
+	</view>
+</template>
+
+<script>
+	export default {
+        props:{
+          goods:{
+              type: Object,
+              default: {}
+          }  
+        },
+		data() {
+			return {
+				// 默认的空图片
+				defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+.goods-item {
+        display: flex;
+        padding: 20rpx 10rpx;
+        border-bottom: 2rpx solid #f0f0f0;
+        .goods-item-left {
+            margin-right: 10rpx;
+            .goods-pic {
+                width: 200rpx;
+                height: 200rpx;
+                display: block;
+            }
+        }
+
+        .goods-item-right {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            .goods-name {
+                font-size: 28rpx;
+            }
+
+            .goods-info-box {
+                .goods-price {
+                    color: #C00000;
+                    font-size: 32rpx;
+                }
+            }
+        }
+    }
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,17 +1,84 @@
 <template>
-	<view>
-		Goods_list
-	</view>
+    <view>
+        <view class="good-list">
+            <view v-for="(goods, index) in goodsList" :key="index" @click="gotoDetail(goods)">
+                <my-goods :goods='goods'></my-goods>
+            </view>
+        </view>
+    </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			};
-		}
-	}
+    export default {
+        data() {
+            return {
+                // 请求参数对象
+                queryObj: {
+                    // 查询关键词
+                    query: '',
+                    // 商品分类Id
+                    cid: '',
+                    // 页码值
+                    pagenum: 1,
+                    // 每页显示多少条数据
+                    pagesize: 10
+                },
+                // 商品列表的数据
+                goodsList: [],
+                // 商品列表的总数量
+                total: 0,
+                // 节流阀
+                isLoading: false
+            };
+        },
+        onLoad(options) {
+            // 将页面参数转存到 this.queryObj 对象中
+            this.queryObj.query = options.query || ''
+            this.queryObj.cid = options.cid || ''
+
+            // 获取商品列表的数据
+            this.getGoodsList()
+        },
+        methods: {
+            async getGoodsList(cb) {
+                // 打开节流阀
+                this.isLoading = true
+                // 发送请求
+                const { data: res } = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+                // 关闭节流阀
+                this.isLoading = false
+                // 当传递了回调函数，就执行
+                cb && cb()
+                if (res.meta.status !== 200) return uni.$showMsg()
+                // 为数据赋值
+                this.goodsList = [...this.goodsList, ...res.message.goods]
+                this.total = res.message.total
+            },
+            gotoDetail(good){
+                uni.navigateTo({url:'/subpkg/goods_detail/goods_detail?goods_id=' + good.goods_id})
+            },
+            // 上拉触底函数
+            onReachBottom() {
+                // 判断是否还有下一页的数据
+                if(this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据加载完毕！')
+                
+                if(this.isLoading) return
+                // 让页面值自增加 1
+                this.queryObj.pagenum++
+                this.getGoodsList()
+            },
+            onPullDownRefresh() {
+                // 重置关键数据
+                this.queryObj.pagenum = 1
+                this.total = 0
+                this.isLoading = false
+                this.goodsList = []
+                
+                // 重新获取商品列表数据
+                this.getGoodsList(()=> uni.stopPullDownRefresh())
+            }
+        }
+    }
 </script>
 
 <style lang="scss">
